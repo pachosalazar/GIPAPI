19d70b977f46121ecb9468430ada2e768d5de10d
Pachosalazar <jfsh87@hotmail.com>
2018-03-24 22:51:09 -0400

    95%

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,19 +5,21 @@ var gifsN = 10;
 
 // create a funtion to display the button
 
-function renderbutton (){
+function renderButtons (){
   for (var i = 0; i < shows.lenght; i++){
    // jquery button  var and code  
-    var button = $("<button>");
-    button.addclass =("btn-group-toggle")
-    button.addclass =("show")
-    button.text(shows[i]);
-    $("#main-buttons").append(button);
+    var newButton = $("<button>");
+    newButton.addClass ("btn")
+    newButton.addClass ("show")
+    newButton.text(shows[i]);
+    $("#main-buttons").append(newButton);
   }
+ 
   //new parameter to remove a previous attachedment
     $(".show").unbind("click");
   //generate on click function to display  conten   
      $(".show").on("click", function(){
+     $(".imaG").unbind("click");
      $("#gifs").empty();
      $("#gifs").removeClass("border border-success");
      myGifs($(this).text())
@@ -25,23 +27,55 @@ function renderbutton (){
 };
 
 //function to add the gits typed in a new button
-function add(newG){
-    if(shows.indexOf(newG)=== -1){
-       shows.push(newG);
+function add(show){
+    if(shows.indexOf(show)=== -1){
+       shows.push(show);
        $("#main-buttons").empty();
-       renderbutton();
+       renderButtons();
     };
 };
 // ajax loooong procces 
-function mychoice(newG){
+function myGifs(show){
     $.ajax({
         // add url and my personal key to all gits
-        url: "https://api.giphy.com/v1/gifs/search?q="+newG+
+        url: "https://api.giphy.com/v1/gifs/search?q="+show+
         "&api_key=zkRGANWyCmsF5J5kgfmVHW6AMLD5NVej"+"&limit="+gifsN,
         method: "GET"
     })
     // i have to set up a response 
     .then(function(response){
-        
-    });  
+      response.data.forEach(function(elemen) {
+        newDiv=$("<div>");
+        newDiv.addClass("individual-gif-container");
+        var newImg =$("<img src='"+elemnt.images.fixed_height-still.url+"'>");
+        newImg.addClass("imaG")
+        newImg.attr("state","still");
+        newImg.attr("still-data",elemnt.images.fixed_height_still.url);
+        newIMG.attr("animated-data",elemnt.images.fixed_height.url);
+        newDiv.append(newImg);
+        $("#gifs").append(newDiv);    
+      }); 
+      //(unbind) removes previous attacheds
+      $("#gifs").addClass("border border-success");
+      $(".imaG").unbind("click");
+      $(".imaG").on("click",function(){
+          if($(this).attr("state")==="still"){
+             $(this).attr("state","animated");
+             $(this).attr("src",$(this).attr("animated-data"));
+          }
+          else {
+             $(this).attr("state","still");
+             $(this).attr("src",$(this).attr("still-data"));
+            }
+	  });
+	});
 }
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		add($("#AMCshow").val().trim());
+		$("#AMCshow").val("");
+	});
+});
